f9498ee5037c8b6b8e87b1d8ac7c69214891f3fa
HridoyHoque <hhridoy155@gmail.com>
2023-06-11 18:48:38 +0600

    Added put method to make user admin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mucefdr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,26 +40,43 @@ async function run() {
             const result = await instructorsCollection.find().toArray();
             res.send(result);
         });
-        // save users email and role in database
-        app.put('/users/:email', async (req, res) => {
+        // get logged users data
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await usersCollection.findOne(query)
+            res.send(result);
+        })
+        // get all users information
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+         // save users email and role in database
+         app.put('/users/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
-            const query = { email: email  }
-            const options = {upsert: true}
+            const query = { email: email }
+            const options = { upsert: true }
             const updateDoc = {
                 $set: user,
             }
             const result = await usersCollection.updateOne(query, updateDoc, options)
             res.send(result);
         })
-
-        // get logged users data
-        app.get('/users/:email', async (req, res) => {
-            const email = req.params.email
-            const query = {email: email}
-            const result = await usersCollection.findOne(query)
+        // make user to admin
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc)
             res.send(result);
         })
+
 
 
         // Send a ping to confirm a successful connection
