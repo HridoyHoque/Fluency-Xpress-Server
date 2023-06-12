52974ab9985cb792c87f86d59192cb9e621d78a5
HridoyHoque <hhridoy155@gmail.com>
2023-06-12 18:14:08 +0600

    used post method to post selectedClassed data

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
 
         const classesCollection = client.db('fluencyDb').collection('classes');
         const newClassesCollection = client.db('fluencyDb').collection('newClasses');
+        const selectedClassesCollection = client.db('fluencyDb').collection('selectedClasses');
         const instructorsCollection = client.db('fluencyDb').collection('instructors');
         const usersCollection = client.db('fluencyDb').collection('users');
 
@@ -89,6 +90,60 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc)
             res.send(result);
         })
+        // make newClasses pending to approved 
+        app.patch('/newClasses/approved/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: 'approved'
+                },
+            }
+            const result = await newClassesCollection.updateOne(filter, updateDoc)
+            res.send(result);
+        })
+        // make newClasses pending to denied 
+        app.patch('/newClasses/denied/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: 'denied'
+                },
+            }
+            const result = await newClassesCollection.updateOne(filter, updateDoc)
+            res.send(result);
+        })
+
+        // get logged instructor classes informationa
+
+       app.get('/newClasses', async (req, res) => {
+        let query = {};
+        if(req.query?.email){
+            query = {email: req.query.email}
+        }
+        const result = await newClassesCollection.find(query).toArray();
+        res.send(result);
+       })
+        // Send feedback to instructor 
+        app.patch('/newClasses/feedback/:id', async (req, res) => {
+            const id = req.params.id;
+            const text = req.body
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    feedback: text
+                },
+            }
+            const result = await newClassesCollection.updateOne(filter, updateDoc)
+            res.send(result);
+        })
+
+        // get all newClasses
+        app.get('/newClasses', async (req, res) => {
+            const result = await newClassesCollection.find().toArray();
+            res.send(result);
+        })
 
         // store newClasses data to db
        app.post('/newClasses', async(req, res) => {
@@ -96,6 +151,14 @@ async function run() {
         const result = await newClassesCollection.insertOne(newClass)
         res.send(result);
        })
+
+
+    //    save selectedClasses data to db
+    app.post('/selectedClasses', async(req, res) => {
+        const selectedClass = req.body;
+        const result = await selectedClassesCollection.insertOne(selectedClass);
+        res.send(result);
+    })
 
 
         // Send a ping to confirm a successful connection
